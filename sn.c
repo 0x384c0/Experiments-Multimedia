00fc90ba312dff87c8e1c0474731520c19b74cf7
Andrew <0x384c0@gmail.com>
2016-06-23 11:26:47 +0600

    added func get_random, modifyFreq

--- sn.c
+++ sn.c
@@ -3,34 +3,52 @@
 #include <stdlib.h>
 #include <math.h>
 
+double modifyFreq(double currentValue);
+double get_random(double min, double max) ;
+
 const int 
 SAMPLING_FREQENCY = 44100,
 INT16T_MAX = 32767,
-DURATION = 2,
-BUFFER_SIZE = BUFSIZ;
+DURATION = 5,
+BUFFER_SIZE = BUFSIZ * 10,
+AMPLITUDE = INT16T_MAX;
+const double
+BASE_FREQ = 0.02;
 
 int16_t         pcm[BUFFER_SIZE];
 
-
 int main() {
-    int 
-    size = SAMPLING_FREQENCY * DURATION,
-    len;
     srand(time(NULL));
-
-    while (size > 0) {
-        len = BUFFER_SIZE;
-
-        double 
-        amplitude = INT16T_MAX,
-        periodDivider =  0.2,
-        periodRandomizer = (rand()%3)-1;
-
-        for (int y = 0; y  < len; y ++) {
-            pcm[y] = (int16_t)(sin(y * M_PI * (periodDivider)) * amplitude);
+    int 
+    sizeLeftTotal = SAMPLING_FREQENCY * DURATION;
+    double 
+    currenfFreq = BASE_FREQ;
+    while (sizeLeftTotal > 0) {
+        for (int y = 0; y  < BUFFER_SIZE; y ++) {
+            pcm[y] = (int16_t)(sin(y * M_PI * (currenfFreq)) * AMPLITUDE);
         }
-
-        size -= fwrite(pcm, sizeof(int16_t), len, stdout); //pipe in to ffmpeg
+        currenfFreq = modifyFreq(currenfFreq);
+        sizeLeftTotal -= fwrite(pcm, sizeof(int16_t), BUFFER_SIZE, stdout); //pipe in to ffmpeg
     }
     return EXIT_SUCCESS;
+}
+
+/* change freq value  between minRand and maxRand*/
+double modifyFreq(double currentValue){
+        double 
+        minRand = -0.03,
+        maxRand = 0.03,
+        freqRandomizer = get_random(minRand, maxRand),
+        result = currentValue + freqRandomizer;
+        if (result > 1) {
+            result = 1 + minRand;
+        }
+        if (result < 0) {
+            result = 0 + maxRand;
+        }
+    return result;
+}
+/* Returns a random double between min and max */
+double get_random(double min, double max) {
+  return (max - min) * ( (double)rand() / (double)RAND_MAX ) + min;
 }
